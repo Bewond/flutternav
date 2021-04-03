c898954544edb134699a797d613337c43a7925a9
Riccardo Brero <riki.brero@gmail.com>
2021-04-03 23:48:35 +0200

    Documentation

--- flutternav/lib/src/elements/nav_element.dart
+++ flutternav/lib/src/elements/nav_element.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 
+/// Base abstract class which is used to implement several [NavElement].
+///
+/// (This class should not be used directly outside the Flutternav package).
 @immutable
 abstract class NavElement<T> {
   const NavElement();
 
+  /// Element of type [T] used in low-level implantation.
   T get baseElement;
 
+  /// Static method to get a list of [baseElement] from one of [NavElement].
   static List<T> toBaseElements<T>(List<NavElement> elements) {
     var result = <T>[];
     for (var element in elements) {
--- flutternav/lib/src/elements/nav_redirector.dart
+++ flutternav/lib/src/elements/nav_redirector.dart
@@ -2,6 +2,10 @@ import 'package:vrouter/vrouter.dart';
 
 import 'package:flutternav/src/elements/nav_element.dart';
 
+/// A [NavElement] which enable to redirect from [path] to [redirect].
+///
+/// Note that this uses [pushReplacement] so if you are on the web,
+/// [path] will not appear in the web history once redirected.
 class NavRedirector extends NavElement<VRouteElement> {
   final String path;
   final String redirect;
