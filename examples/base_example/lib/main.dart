695886593bc1f49025d39af43d8f76b720302141
Riccardo Brero <riki.brero@gmail.com>
2021-04-28 16:16:03 +0200

    Example

--- examples/base_example/lib/main.dart
+++ examples/base_example/lib/main.dart
@@ -10,29 +10,36 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var routes = [
-      NavRoute(
-        path: '/',
-        widget: MainScreen(),
-        stacked: [
-          NavRoute(path: 'details/:id', widget: DetailsScreen()),
-        ],
-      ),
-      NavSubRoute(
-        path: '/menu',
-        build: (child) => MenuScreen(page: child),
-        nested: [
-          NavRoute(path: 'page1', widget: Page1()),
-          NavRoute(path: 'page2', widget: Page2()),
-        ],
-      ),
-      NavRoute(path: '/404', widget: UnknownScreen()),
-      NavRedirector(path: ':_(.+)', redirect: '/404'),
-    ];
+    NavRouter router = NavRouter(
+      routes: [
+        NavRoute(
+          path: '/',
+          widget: MainScreen(),
+          stacked: [
+            NavRoute(path: 'details/:id', widget: DetailsScreen()),
+          ],
+        ),
+        NavSubRoute(
+          path: '/menu',
+          build: (child) => MenuScreen(page: child),
+          nested: [
+            NavRoute(path: 'page1', widget: Page1()),
+            NavRoute(path: 'page2', widget: Page2()),
+          ],
+        ),
+        NavRoute(path: '/404', widget: UnknownScreen()),
+        NavRedirector(path: ':_(.+)', redirect: '/404'),
+      ],
+      initialUrl: '/',
+      routerMode: NavRouterModes.history,
+    );
 
     return MaterialApp.router(
-      routeInformationParser: VRouteInformationParser(),
-      routerDelegate: routerDelegate,
+      title: 'Flutternav Demo',
+      debugShowCheckedModeBanner: false,
+      routeInformationParser: router.routeInformationParser,
+      routerDelegate: router.routerDelegate,
+      backButtonDispatcher: router.backButtonDispatcher,
     );
   }
 }
